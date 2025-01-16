c3f9f96abd6a12f6358e84deb2d0ffb92fbd6f58
Dez E. <dezebuwa@gmail.com>
2025-01-16 12:18:28 -0500

    Update virtual_machines.tf

--- tf-gen/base/virtual_machines.tf
+++ tf-gen/base/virtual_machines.tf
@@ -25,3 +25,26 @@ resource "azurerm_linux_virtual_machine" "rhel9_vms" {
     public_key = file("~/.ssh/id_rsa.pub")
   }
 }
+
+resource "azurerm_windows_virtual_machine" "win19_vm" {
+  name                 = "win19-server"
+  location             = azurerm_resource_group.newteam_rg.location
+  resource_group_name  = azurerm_resource_group.newteam_rg.name
+  size                 = "Standard_B2s"
+  admin_username       = "adminuser"
+  admin_password       = "P@ssword1234!"
+  network_interface_ids = [azurerm_network_interface.newteam_nic[3].id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "win19-server-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
